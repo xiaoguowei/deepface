d0b4953d0f79cb959159be307f953ed14dec75cb
annielee <annie.lee@wustl.edu>
2018-08-02 17:39:10 +0900

    train and evaluation method added + saved batch normalization update ops for evaluation mode.

--- resnet_train/model_fn.py
+++ resnet_train/model_fn.py
@@ -15,21 +15,21 @@ def conv_block(input_tensor, filters, stage, block, mode, strides=(2, 2), bias=F
     layer_name = 'conv' + str(stage) + '_' + str(block)
     l = tf.layers.conv2d(input_tensor, filters[0], 1, strides=strides, use_bias=bias,
                          name=layer_name + '_1x1_reduce')
-    l = tf.layers.batch_normalization(l, axis=3, name=layer_name + '_1x1_reduce/bn',
+    l = tf.layers.batch_normalization(l, axis=3, momentum=0.9, name=layer_name + '_1x1_reduce/bn',
                                       training=mode == tf.estimator.ModeKeys.TRAIN)
     l = tf.nn.relu(l)
 
     l = tf.layers.conv2d(l, filters[1], 3, padding='SAME', use_bias=bias, name=layer_name + '_3x3')
-    l = tf.layers.batch_normalization(l, axis=3, name=layer_name + '_3x3/bn',
+    l = tf.layers.batch_normalization(l, axis=3, momentum=0.9, name=layer_name + '_3x3/bn',
                                       training=mode == tf.estimator.ModeKeys.TRAIN)
     l = tf.nn.relu(l)
 
     l = tf.layers.conv2d(l, filters[2], 1, name=layer_name + '_1x1_increase')
-    l = tf.layers.batch_normalization(l, axis=3, name=layer_name + '_1x1_increase/bn',
+    l = tf.layers.batch_normalization(l, axis=3, momentum=0.9, name=layer_name + '_1x1_increase/bn',
                                       training=mode == tf.estimator.ModeKeys.TRAIN)
 
     m = tf.layers.conv2d(input_tensor, filters[2], 1, strides=strides, use_bias=bias, name=layer_name + '_1x1_proj')
-    m = tf.layers.batch_normalization(m, axis=3, name=layer_name + '_1x1_proj/bn',
+    m = tf.layers.batch_normalization(m, axis=3, momentum=0.9, name=layer_name + '_1x1_proj/bn',
                                       training=mode == tf.estimator.ModeKeys.TRAIN)
 
     l = tf.add(l, m)
@@ -42,16 +42,16 @@ def identity_block(input_tensor, filters, stage, block, mode, bias=False):
 
     layer_name = 'conv' + str(stage) + '_' + str(block)
     l = tf.layers.conv2d(input_tensor, filters[0], 1, use_bias=bias, name=layer_name + '_1x1_reduce')
-    l = tf.layers.batch_normalization(l, axis=3, name=layer_name + '_1x1_reduce/bn',
+    l = tf.layers.batch_normalization(l, axis=3, momentum=0.9, name=layer_name + '_1x1_reduce/bn',
                                       training=mode == tf.estimator.ModeKeys.TRAIN)
     l = tf.nn.relu(l)
 
     l = tf.layers.conv2d(l, filters[1], 3, padding='SAME', use_bias=bias, name=layer_name + '_3x3')
-    l = tf.layers.batch_normalization(l, name=layer_name + '_3x3/bn', training=mode == tf.estimator.ModeKeys.TRAIN)
+    l = tf.layers.batch_normalization(l, momentum=0.9, name=layer_name + '_3x3/bn',
+                                      training=mode == tf.estimator.ModeKeys.TRAIN)
     l = tf.nn.relu(l)
-
     l = tf.layers.conv2d(l, filters[2], 1, use_bias=bias, name=layer_name + '_1x1_increase')
-    l = tf.layers.batch_normalization(l, name=layer_name + '_1x1_increase/bn',
+    l = tf.layers.batch_normalization(l, momentum=0.9, name=layer_name + '_1x1_increase/bn',
                                       training=mode == tf.estimator.ModeKeys.TRAIN)
 
     l = tf.add(l, input_tensor)
@@ -70,7 +70,9 @@ def resnet_model_fn(features, labels, mode):
     # Building hidden layers (ResNet architecture)
     # First block:
     l = tf.layers.conv2d(input_layer, 64, (7, 7), strides=(2, 2), padding='SAME', use_bias=False, name='conv1/7x7_s2')
-    l = tf.layers.batch_normalization(l, axis=3, name='conv1/7x7_s2/bn', training=mode == tf.estimator.ModeKeys.TRAIN)
+    l = tf.layers.batch_normalization(l, axis=3, momentum=0.9, name='conv1/7x7_s2/bn',
+                                      training=mode == tf.estimator.ModeKeys.TRAIN)
+
     l = tf.nn.relu(l)
     l = tf.layers.max_pooling2d(l, 3, 2)
 
@@ -78,7 +80,6 @@ def resnet_model_fn(features, labels, mode):
     l = conv_block(l, [64, 64, 256], stage=2, block=1, mode=mode, strides=(1, 1))
     l = identity_block(l, [64, 64, 256], stage=2, block=2, mode=mode)
     l = identity_block(l, [64, 64, 256], stage=2, block=3, mode=mode)
-    # tf.summary.image('after_second_layer', l)
 
     # Third block:
     l = conv_block(l, [128, 128, 512], stage=3, block=1, mode=mode)
@@ -122,25 +123,29 @@ def resnet_model_fn(features, labels, mode):
     l2_loss = WEIGHT_DECAY * tf.add_n([tf.nn.l2_loss(tf.cast(v, tf.float32)) for v in tf.trainable_variables()])
     loss = cross_entropy + l2_loss
 
-    # Piecewise constant learning rate:
-    learning_rate = tf.train.piecewise_constant(tf.train.get_global_step(), [120000, 240000, 300000, 480000],
-                                                [0.2, 0.15, 0.1, 0.01, 0.001])
-
     # Logging tensor hook
-    tf.identity(learning_rate, 'learning_rate')
     tf.identity(loss, 'cross_entropy')
     tf.identity(accuracy[1], name='train_accuracy')
     tf.identity(labels, 'true_labels')
-    tf.summary.scalar('learning_rate', learning_rate)
-    tf.summary.scalar('accuracy', accuracy[1])
 
     # Configure the Training Op
     if mode == tf.estimator.ModeKeys.TRAIN:
+        # Piecewise constant learning rate:
+        learning_rate = tf.train.piecewise_constant(tf.train.get_global_step(), [120000, 240000, 300000, 480000],
+                                                    [0.2, 0.15, 0.1, 0.01, 0.001])
+        tf.identity(learning_rate, 'learning_rate')
+        tf.summary.scalar('learning_rate', learning_rate)
+
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
         optimizer = tf.contrib.estimator.TowerOptimizer(optimizer)
-        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+
+        # Save update_ops for batch_normalization
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
     # Add evaluation metrics
     eval_metric_ops = {"accuracy": accuracy}
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    tf.summary.scalar('accuracy', accuracy[1])
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops, predictions=predictions)
