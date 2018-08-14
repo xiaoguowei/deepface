9ea9254b7283c4dacd3c158762ecdff911635adf
ildoonet <ildoo@ildoo.net>
2018-08-14 16:40:58 +0900

    add new saved_model.

--- deepface/recognizers/vggface2_resnet/download.sh
+++ deepface/recognizers/vggface2_resnet/download.sh
@@ -12,3 +12,6 @@ extract_download_url() {
 
 wget $( extract_download_url http://www.mediafire.com/file/datqeencsrqua22/rcmalli_vggface_tf_resnet50.h5 ) -O $DIR/weight.h5
 wget $( extract_download_url http://www.mediafire.com/file/sc2fo5xcktwbqkd/rcmalli_vggface_labels_v2.npy ) -O $DIR/labels.npy
+
+wget $( extract_download_url http://www.mediafire.com/file/a550w53sqykwos2/saved_model_180814.tar.gz ) -O $DIR/saved_model.tar.gz
+tar -xvf saved_model.tar.gz
